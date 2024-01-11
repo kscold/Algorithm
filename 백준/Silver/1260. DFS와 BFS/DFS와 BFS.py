e113b9aa0fad8ce92cc5d057f4fdd36da44202f2
KIM SEUNG CHAN <66587554+kscold@users.noreply.github.com>
2024-01-11 16:56:00 +0900

    [Silver II] Title: DFS와 BFS, Time: 72 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,44 +1,45 @@
 from collections import deque
+import sys
 
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
 
-def dfs(start):
-    visited[start] = True
-    print(start, end=" ")
-
-    for i in graph[start]:
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    for i in graph[v]:
         if not visited[i]:
-            dfs(i)
+            dfs(graph, i, visited)
 
-
-def bfs(start):
+def bfs(graph, start, visited):
     queue = deque([start])
     visited[start] = True
-    while queue:
 
+    while queue:
         v = queue.popleft()
-        print(v, end=" ")
+        print(v, end=' ')
         for i in graph[v]:
             if not visited[i]:
-                visited[i] = True
                 queue.append(i)
+                visited[i] = True
+
+n, m, v = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
 
 
-N, M, V = map(int, input().split())
-graph = [[] for _ in range(N + 1)]
-
-for _ in range(M):
+for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-
 for i in graph:
     i.sort()
 
-
-visited = [False] * (N + 1)
-dfs(V)
+visited1 = [False] * (n + 1)
+dfs(graph, v, visited1)
 print()
 
-visited = [False] * (N + 1)
-bfs(V)
+visited2 = [False] * (n + 1)
+bfs(graph, v, visited2)
+
