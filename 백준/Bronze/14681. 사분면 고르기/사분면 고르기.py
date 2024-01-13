edb6b64b984073b3df533d33e6bd4f8428d39223
KIM SEUNG CHAN <66587554+kscold@users.noreply.github.com>
2024-01-14 00:13:41 +0900

    [Bronze V] Title: 사분면 고르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,14 @@
+def innerpredict(x, y):
+    if x > 0 and y > 0:
+        return 1
+    elif x < 0 and y > 0:
+        return 2
+    elif x < 0 and y < 0:
+        return 3
+    elif x > 0 and y < 0:
+        return 4
+
+
+x = int(input())
+y = int(input())
+print(innerpredict(x, y))
