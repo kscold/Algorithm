09c2cc69577f681b6b14fa878a6f444ae9ef9f0c
KIM SEUNG CHAN <66587554+kscold@users.noreply.github.com>
2024-01-14 00:00:44 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+target = int(input())
+if 90 <= target <= 100:
+    print("A")
+elif 80 <= target <= 89:
+    print("B")
+elif 70 <= target <= 79:
+    print("C")
+elif 60 <= target <= 69:
+    print("D")
+else:
+    print("F")
